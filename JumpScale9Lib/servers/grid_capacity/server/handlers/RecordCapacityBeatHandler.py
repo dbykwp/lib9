bb8d7ca968dd4e1a6108754b21c18ce951598421
Ahmed T. Youssef <xmonader@gmail.com>
2018-07-18 16:31:43 +0200

    Update server for heartbeats

--- JumpScale9Lib/servers/grid_capacity/server/handlers/RecordCapacityBeatHandler.py
+++ JumpScale9Lib/servers/grid_capacity/server/handlers/RecordCapacityBeatHandler.py
@@ -16,15 +16,15 @@ from ..grid_stats import push_node_beat
 from ..flask_itsyouonline import ITSYOUONLINE_KEY
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-Capacity_schema = JSON.load(open(dir_path + '/schema/Capacity_schema.json'))
-Capacity_schema_resolver = jsonschema.RefResolver('file://' + dir_path + '/schema/', Capacity_schema)
-Capacity_schema_validator = Draft4Validator(Capacity_schema, resolver=Capacity_schema_resolver)
+Heartbeat_schema = JSON.load(open(dir_path + '/schema/Heartbeat_schema.json'))
+Heartbeat_schema_resolver = jsonschema.RefResolver('file://' + dir_path + '/schema/', Heartbeat_schema)
+Heartbeat_schema_validator = Draft4Validator(Heartbeat_schema, resolver=Heartbeat_schema_resolver)
 
 
 def RecordCapacityBeatHandler():
     inputs = request.get_json()
     # refresh jwt is needed otherwise return original
-    jwt = j.clients.itsyouonline.refresh_jwt_token(inputs.pop('farmer_id'))
+    jwt = j.clients.itsyouonline.refresh_jwt_token(inputs.get('farmer_id'))
     token = jose.jwt.decode(jwt, ITSYOUONLINE_KEY)
     iyo_organization = token['scope'][0].replace('user:memberof:', '')
     farmer = Farmer.objects(iyo_organization=iyo_organization).first()
@@ -32,14 +32,18 @@ def RecordCapacityBeatHandler():
         return jsonify(errors='Unauthorized farmer'), 403
 
     try:
-        Capacity_schema_validator.validate(inputs)
+        Heartbeat_schema_validator.validate(inputs)
     except jsonschema.ValidationError as e:
         return jsonify(errors="bad request body: {}".format(e)), 400
-    
+
+
+    inputs.pop('farmer_id')
+
+    # heartbeat is subset of capacity.
     inputs['farmer'] = iyo_organization
     inputs['updated'] = datetime.now()
 
     capacity = Capacity(**inputs)
 
     push_node_beat(farmer.iyo_organization, capacity.node_id)
-    return capacity.to_json(use_db_field=False), 201, {'Content-type': 'application/json'}
+    return '', 204
